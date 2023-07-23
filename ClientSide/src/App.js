62646a788a3786e09dcd871bdce1d138752a644b
Waseem468 <waseemashrafi1344@gmail.com>
2023-07-23 20:10:20 +0530

    product page and api related to product page and some changes in frontend done

--- ClientSide/src/App.js
+++ ClientSide/src/App.js
@@ -1,13 +1,42 @@
-import AppRouter from './AppRouter';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './component/LandingPage';
+import Products from './component/Products';
+import UserRegister from './component/User/UserRegister';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-  <AppRouter/>
-    </div>
-  );
-}
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    async function getAllProducts() {
+      try {
+        const response = await fetch(`http://localhost:5000/product/`);
+        const data = await response.json();
+        if (data && Array.isArray(data.products)) {
+          setItems(data.products);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    getAllProducts();
+  }, []);
+  console.log(items);
+
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path={'/'} element={<LandingPage />} />
+          <Route path={'/userRegister'} element={<UserRegister />} />
+          <Route path={'/product'} element={<Products items={items} />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
 export default App;
--- ServerSide/app.js
+++ ServerSide/app.js
@@ -2,13 +2,16 @@ const express = require("express")
 const app = express();
 const cors = require("cors")
 require("./src/DB/server")
+
 const UserRouter = require("./src/Routers/UserRouter")
+const ProductRouter=require("./src/Routers/ProductRouters")
 
 app.use(express.json())
 app.use(cors());
 app.use(express.urlencoded({extended:true}))
 
-app.use('/user', UserRouter)
+app.use('/user', UserRouter);
+app.use('/product',ProductRouter);
 
 
 app.listen(5000, ()=>{
